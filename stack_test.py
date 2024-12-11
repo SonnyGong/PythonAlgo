fd69fc520deff90714304593db618edc867243db
canyang.gong <canyang.gong@zongmutech.com>
2024-12-11 17:47:33 +0800

    LIFO

--- stack_test.py
+++ stack_test.py
@@ -22,6 +22,9 @@
 ------------      -----------    --------    -----------
 2024/12/10 17:13   NotToday      1.0         None
 '''
+import string
+
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -96,6 +99,7 @@ def divideBy2(decNumber):
         rem = decNumber % 2
         remstack.push(rem)
         decNumber = decNumber // 2
+        print(rem,decNumber)
 
     binString = ""
     while not remstack.isEmpty():
@@ -103,7 +107,59 @@ def divideBy2(decNumber):
 
     return binString
 
+def divideByAny(decNumber,base):
+    digits = "0123456789ABCDEF"
+    remstack = Stack()
+    while decNumber > 0:
+        rem = decNumber % base
+        remstack.push(rem)
+        decNumber = decNumber // base
+
+
+    binString = ""
+    while not remstack.isEmpty():
+        binString += digits[remstack.pop()]
+
+    return binString
+
+
+def infixToPostfix(infix):
+    pre_dict = {}
+    pre_dict['*'] = 3
+    pre_dict['/'] = 3
+    pre_dict['+'] = 2
+    pre_dict['-'] = 2
+    pre_dict['('] = 1
+    return_list = []
+    temp_stack = Stack()
+    _tempinfix = infix.split()
+
+    for thisstr in _tempinfix:
+        if thisstr in string.ascii_uppercase:
+            return_list.append(thisstr)
+        elif thisstr == '(':
+            temp_stack.push(thisstr)
+        elif thisstr == ')':
+            top_str = temp_stack.pop()
+            while top_str != '(':
+                return_list.append(top_str)
+                top_str = temp_stack.pop()
+        else:
+            while not temp_stack.isEmpty() and pre_dict[thisstr] <= pre_dict[temp_stack.peek()]:
+                return_list.append(temp_stack.pop())
+            temp_stack.push(thisstr)
+
+    while not temp_stack.isEmpty():
+        return_list.append(temp_stack.pop())
+
+    return "".join(return_list)
+
+
+
+
 if __name__ == '__main__':
     pass
+    # print(divideByAny(233,16))
     # test = "()()()({(}))"
     # print(parChecker_2(test))
+    print(infixToPostfix("( A + B ) * ( C + D )") )
