6b5208241e3ec6e768bf0f912bab1f45eeb41b47
canyang.gong <canyang.gong@zongmutech.com>
2024-12-16 17:51:28 +0800

    2024年12月16日

--- DataStructures_Algorithms/4-递归/07-找零问题.py
+++ DataStructures_Algorithms/4-递归/07-找零问题.py
@@ -52,21 +52,60 @@ def print_coins(change, coin_used):
         coins.append(coin)
     print(', '.join(str(coin) for coin in coins))
 
+def dpMakeChange(coinValueList,change,minCoins):
+    for cents in range(change + 1):
+        coinCount = cents
+        for j in [c for c in coinValueList if c <= cents]:
+            coinCount = minCoins[cents - j] +1
+        minCoins[cents] = coinCount
+
+    return minCoins[change]
+
+
+memo = {}
+def L(nums, i):
+    if i in memo:
+        return memo[i]
+
+    if i == len(nums) - 1:
+        return 1
+
+    max_len = 1
+    for j in range(i+1,len(nums)):
+        if nums[j] > nums[i]:
+            max_len = max(max_len, L(nums, j) + 1)
+    memo[i] = max_len
+    return max_len
+
+def length_of_longest(nums):
+    return max(L(nums,i) for i in range(len(nums)))
+
+def length_of_LIS(nums):
+    n = len(nums)
+    L = [1] * n
+    for i in reversed(range(n)):
+        for j in range(i + 1, n):
+            if nums[j] > nums[i]:
+                L[i] = max(L[i], L[j] + 1)
+
+    return max(L)
 
 if __name__ == '__main__':
-    print(rec_make_change([1, 5, 10, 25], 63))
-    print(dp_make_change([1, 5, 10, 25], 63)[0])
-    print(dp_make_change_2([1, 5, 10, 25], 63)[0])
-    print('*' * 50)
-
-    cl = [1, 5, 10, 21, 25]
-
-    _, coin_used = dp_make_change(cl, 63)
-    print_coins(63, coin_used)
-    print_coins(52, coin_used)
-
-    print('*' * 50)
-
-    _, coin_used_2 = dp_make_change_2(cl, 63)
-    print_coins(63, coin_used_2)
-    print_coins(52, coin_used_2)
+    print(length_of_longest([1,5,2,4,3]))
+    # print(dpMakeChange([1,2,3,4,5],63, {}))
+    # print(rec_make_change([1, 5, 10, 25], 63))
+    # print(dp_make_change([1, 5, 10, 25], 63)[0])
+    # print(dp_make_change_2([1, 5, 10, 25], 63)[0])
+    # print('*' * 50)
+    #
+    # cl = [1, 5, 10, 21, 25]
+    #
+    # _, coin_used = dp_make_change(cl, 63)
+    # print_coins(63, coin_used)
+    # print_coins(52, coin_used)
+    #
+    # print('*' * 50)
+    #
+    # _, coin_used_2 = dp_make_change_2(cl, 63)
+    # print_coins(63, coin_used_2)
+    # print_coins(52, coin_used_2)
--- DataStructures_Algorithms/5-搜索和排序/04-冒泡排序.py
+++ DataStructures_Algorithms/5-搜索和排序/04-冒泡排序.py
@@ -4,11 +4,16 @@
 def bubble_sort(arr):
     n = len(arr)
     for pass_num in range(n - 1, 0, -1):
+        print(pass_num)
         for i in range(pass_num):
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
     return arr
+
+
+
+
 
 
 # 短冒牌排序
@@ -26,9 +31,23 @@ def short_bubble_sort(arr):
     return arr
 
 
+def test_bubble_sort(arr):
+    length = len(arr)
+    for i in range(length - 1,0,-1):
+        if_exchange = False
+        for single_turn in range(i):
+            if arr[single_turn] > arr[single_turn + 1]:
+                arr[single_turn + 1], arr[single_turn] = arr[single_turn], arr[single_turn + 1]
+                if_exchange = True
+
+        if not if_exchange:
+            break
+    return arr
+
+
 if __name__ == '__main__':
     arr = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    seq = bubble_sort(arr[:])
+    seq = test_bubble_sort(arr[:])
     print(f'冒泡排序结果: {seq}')
-    seq2 = short_bubble_sort(arr[:])
-    print(f'短冒泡排序结果: {seq2}')
+    # seq2 = short_bubble_sort(arr[:])
+    # print(f'短冒泡排序结果: {seq2}')
